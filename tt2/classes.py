b60158471b6770d5e8e1c99e6999744c92d9251f
JakubPonulak <89219726+JakubPonulak@users.noreply.github.com>
2022-03-23 05:59:23 +0000

    last minuscule update

--- tt2/classes.py
+++ tt2/classes.py
@@ -61,7 +61,7 @@ class Palindrome:
       if l1 == l2:
         pali_true += 1
       else:
-        print("An error occurs at", self.letters[len(self.letters) - i - 1], "which is letter ", len(self.letters) - i, ". This letter should be", self.letters[i])
+        print("An error occurs at", self.letters[len(self.letters) - i - 1], "which is letter", len(self.letters) - i, "- This letter should be", self.letters[i])
     if pali_true == int(self.length / 2):
       print("This is a palindrome!")
     else:
@@ -69,15 +69,15 @@ class Palindrome:
 
 def pali_print():
   test_data1 = Palindrome(user_input='Racecar')
-  test_data2 = Palindrome(user_input='A man, a plan, a canal - Panama!')
+  test_data2 = Palindrome(user_input='A man, a plan, a canal - Panama?')
   test_data3 = Palindrome(user_input='Test text')
-  test_data4 = Palindrome(user_input='Slash slash!')
+  test_data4 = Palindrome(user_input='Slash slash slash')
   print("Phrase 1: Racecar")
   test_data1()
-  print("Phrase 2: A man, a plan, a canal - Panama!")
+  print("Phrase 2: A man, a plan, a canal - Panama?")
   test_data2()
   print("Phrase 3: Test text")
   test_data3()
-  print("Phrase 4: Slash slash!")
+  print("Phrase 4: Slash slash slash")
   test_data4()
 
