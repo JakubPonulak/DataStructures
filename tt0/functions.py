db13e526bea93471fab2b3e4a843d6be8cf849ec
JakubPonulak <89219726+JakubPonulak@users.noreply.github.com>
2022-03-23 05:51:19 +0000

    palindrome class (points points points)

--- tt0/functions.py
+++ tt0/functions.py
@@ -87,6 +87,7 @@ sub_menu2 = [
   ["Factorial", classes.factorial_print],
   ["Sequential Sum (OOP)", classes.consec_print],
   ["Sequential Sum (Imperative)", classes.seq_sum],
+  ["Palindrome", classes.pali_print],
 ]
 
 # Menu banner is typically defined by menu owner
--- tt2/classes.py
+++ tt2/classes.py
@@ -1,3 +1,5 @@
+import re
+
 class Factorial:
   def __init__(self, num):
     self.num = num 
@@ -42,5 +44,40 @@ class Consec_Nums:
 
 def consec_print():
   inp = int(input("Enter a number you want to find the sequential sum of: "))
-  Consec_Nums(inp).__call__()
-      
+  func = Consec_Nums(inp)
+  func()
+
+class Palindrome:
+  def __init__(self, user_input):
+    self.user_input = user_input
+    self.letters = re.sub("[^a-zA-Z]+", "", self.user_input)
+    self.length = len(self.letters)
+  def __call__(self):
+    range_text = int(self.length / 2)
+    pali_true = 0
+    for i in range(0, range_text):
+      l1 = self.letters[i].lower()
+      l2 = self.letters[len(self.letters) - i - 1].lower()
+      if l1 == l2:
+        pali_true = 1
+      else:
+        print("An error occurs at", self.letters[len(self.letters) - i - 1], "which is letter ", len(self.letters) - i, ". This letter should be", self.letters[i])
+    if pali_true == 1:
+      print("This is a palindrome!")
+    else:
+      print("This is not a palindrome...")
+
+def pali_print():
+  test_data1 = Palindrome(user_input='Racecar')
+  test_data2 = Palindrome(user_input='A man, a plan, a canal - Panama!')
+  test_data3 = Palindrome(user_input='Test text')
+  test_data4 = Palindrome(user_input='Slash slash!')
+  print("Phrase 1: Racecar")
+  test_data1()
+  print("Phrase 2: A man, a plan, a canal - Panama!")
+  test_data2()
+  print("Phrase 3: Test text")
+  test_data3()
+  print("Phrase 4: Slash slash!")
+  test_data4()
+
